e2935ce88f8a197ea7578f2c56faf7b774727532
jamesknelson <james@jamesknelson.com>
2015-07-14 21:05:17 +0900

    initial commit

---
+++ src/RenderInBody.js
@@ -0,0 +1,33 @@
+import React, {PropTypes, DOM} from "react"
+import ReactDOM from "react-dom"
+
+
+export default class RenderInBody extends Base {
+  componentDidMount() {
+    this.popup = document.createElement("div")
+    document.body.appendChild(this.popup)
+    this.renderLayer()
+  }
+
+
+  componentDidUpdate() {
+    this.renderLayer()
+  }
+
+
+  componentWillUnmount() {
+    ReactDOM.unmountComponentAtNode(this.popup)
+    document.body.removeChild(this.popup)
+  }
+
+
+  renderLayer() {
+    ReactDOM.render(this.props.children, this.popup)
+  }
+
+
+  render() {
+    // Render a placeholder
+    return <div {...this.props} children={null} />
+  }
+}
